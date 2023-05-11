2a3d59007e098670259b0193c06d753c75834919
unknown <annieyashley@gmail.com>
2023-05-10 20:06:20 -0500

    sets up some controllers

---
+++ controllers/home_routes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Blog, Comment, User } = require('../models');
+const sequelize = require("../config/connection")
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+      // Get all projects and JOIN with user data
+      const blogData = await Blog.findAll({
+        include: [
+          {
+            model: Comment,
+            attributes: ['id', 'content', 'date', 'user_id', 'blog_id'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const blogs = blogData.map((blog) => blog.get({ plain: true }));
+      console.log(blogs)
+  
+      // Pass serialized data and session flag into template
+      res.render('homepage', { 
+        blogs, 
+        logged_in: req.session.logged_in 
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
